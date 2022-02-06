275889b28fb2ff17822fbf8c7aacb4e9b99b286d
Harshvardhan P. Joshi <hpjoshi@gmail.com>
2022-02-06 15:45:08 -0500

    Rename apping to apdelay and cleanup and add comments

--- src/apdelay.py
+++ src/apdelay.py
@@ -1,21 +1,25 @@
 import pingparser
 from ap_utils import *
 
-class apping:
+class apdelay:
     """
-    Measure latency between two nodes.
+    Measure latency between two nodes: localhost and destip.
+    If multi-homed the source interface can be specified with srcip.
+    The pings are repeated count times, and interval seconds are
+    the time between each ping packet send.
     """
     def __init__ (self, destip, srcip=None, count=3, interval=0.2):
         self.srcip = srcip
         self.destip = destip
         self.count = count
         self.interval = interval
+        self.pType = "ping"
         self.output = None
-        self.dict = None
+        self.pDict = None
 
     def ping(self):
         """
-        Run ping to a host and return output and returncode
+        Run ping to a host and return returncode and output
         """
         command = "ping -c%d %s" % (self.count, self.destip)
         if self.interval is not None:
@@ -27,11 +31,20 @@ class apping:
         return ret, self.output
 
 
+    def owping(self):
+        """
+        One-way ping using OWAMP tools from perfSonar.
+        Currently not supported.
+        """
+        self.pType = "owping"
+        return None
+
+
     def parse(self):
         """
         Return the output of ping parsed as a dictionary object
         """
         if self.output is not None:
-            self.dict = pingparser.parse(self.output)
-        return self.dict
+            self.pDict = pingparser.parse(self.output)
+        return self.pDict
 
